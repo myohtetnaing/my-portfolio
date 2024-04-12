768012961511cc3f4fb37fe2e8f89cb73a54a045
myohtetnaing <myohtetnaing.dev@gmail.com>
2024-04-12 22:13:11 +0630

    Update Feature

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -5,6 +5,22 @@ const title = "Myo Htet Naing - A Full Stack Developer"
 router.get('/',(req,res)=>{
     res.render('./pages/index.ejs',{title})
 })
+<<<<<<< HEAD
+=======
+router.get('/whoami',(req,res)=>{
+    res.redirect('/#whoami')
+})
+router.get('/project',(req,res)=>{
+    res.redirect('/#projects')
+})
+router.get('/projects',(req,res)=>{
+    res.redirect('/#projects')
+})
+router.get('/blog',(req,res)=>{
+    res.redirect('/#blog')
+})
+
+>>>>>>> 4d3b7e8 (Update Some Feature)
 
 router.use((req, res) => { 
     res.status(404).render('./pages/notFound',{title})
